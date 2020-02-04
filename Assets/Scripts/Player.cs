36a6c448bda2b6db069b1a4acb59c7d2a5aba9d1
Blake <telefragent@gmail.com>
2020-02-04 13:12:30 -0500

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public SpriteRenderer PlayerSprite;
+	public Sprite Sprite1;
+	public Sprite Sprite2;
+	public float PlayerSpeed;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.position += new Vector3(-PlayerSpeed, 0f, 0f);
+		}
+		else if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.position += new Vector3(PlayerSpeed, 0f, 0f);
+		}
+
+		if(transform.position.x > 0f)
+		{
+			PlayerSprite.sprite = Sprite2;
+		}
+		else if (transform.position.x < 0f)
+		{
+			PlayerSprite.sprite = Sprite1;
+		}
+
+	}
+}
