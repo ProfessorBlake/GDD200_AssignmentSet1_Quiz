e7d1991c2f57ffa00d1a9370a6f92f0a2656fd54
Blake <telefragent@gmail.com>
2020-02-04 13:33:39 -0500

    quiz setup

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,36 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-	public SpriteRenderer PlayerSprite;
-	public Sprite Sprite1;
-	public Sprite Sprite2;
-	public float PlayerSpeed;
-
-    void Start()
-    {
-        
-    }
-
+	public float PlayerSpeed = 0.05f;
 
     void Update()
     {
-		if (Input.GetKey(KeyCode.LeftArrow))
-		{
-			transform.position += new Vector3(-PlayerSpeed, 0f, 0f);
-		}
-		else if (Input.GetKey(KeyCode.RightArrow))
-		{
-			transform.position += new Vector3(PlayerSpeed, 0f, 0f);
-		}
-
-		if(transform.position.x > 0f)
-		{
-			PlayerSprite.sprite = Sprite2;
-		}
-		else if (transform.position.x < 0f)
-		{
-			PlayerSprite.sprite = Sprite1;
-		}
-
+		
 	}
 }
